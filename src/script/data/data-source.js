dfab0872c329e2285a79497cfff1ec730bb1b4a9
azzam <azzam.jiul@gmail.com>
2020-07-31 19:04:03 +0700

    promise

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,18 +1,14 @@
 class DataSource {
-    constructor (onSuccess, onFailed) {
-        this.onSuccess = onSuccess;
-        this.onFailed = onFailed;
-    }
+    static searchClub (keyword) {
 
-    searchClub (keyword) {
-        const filteredClubs = clubs.filter(club => {
-            return club.name.toUpperCase().includes(keyword.toUpperCase());
+        return new Promise((resolve, reject) => {
+            const filteredClubs = clubs.filter(club => {return club.name.toUpperCase().includes(keyword.toUpperCase());})
+
+            if (filteredClubs.length) {
+                resolve(filteredClubs);
+            } else {
+                reject(`${keyword} is not found`);
+            }
         })
-
-        if (filteredClubs.length) {
-            this.onSuccess(filteredClubs);
-        } else {
-            this.onFailed(`${keyword} is not found`);
-        }
     }
 }
